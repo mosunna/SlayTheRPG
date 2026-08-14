2b3808f9435ae9ff7ea55db9bb3059e8ac62920e
Mosunna Landaisgit config --listgit --versionclear <mosunnal@gmail.com>
2026-08-13 21:57:11 -0400

    Added ExecuteIntent method to Enemy.cs script, it will dictate the outcome of the enemies and actually carry out their intented action with CombatActions.cs

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -9,8 +9,15 @@ public class Enemy : Character
     //Decides this enemy's next action and stores it as CurrentIntent, to be read and executed during ENEMY_TURN
     public void ChooseNextIntent(Character target)
     {
-        //TODO: branch on enemy type once more than Slime's attack-only behavior exists
+        //TODO: branch on enemy type once more than an acttack only behavior exists
         //TODO: assign a real icon once the IntentIcon system exists in Phase 3
         CurrentIntent = new Intent(IntentType.Attack, attack, target, null);
+    }
+
+    //Executes the enemy's current intention, decisin will be applied through CombatActions
+    public void ExecuteIntent()
+    {
+        //If statements on the three possibiltiies of enemy intent (what they plan to do on their turn)
+        //Where if buff is suppose to be executed, it will have different branches depending on the character type casting it
     }
 }
